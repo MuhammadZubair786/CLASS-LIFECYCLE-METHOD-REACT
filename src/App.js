5221c5c57c484f5d1ab0c7d3d7a5d893951d3242
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2021-03-24 01:36:15 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import React,{Component} from 'react'
 import './App.css';
 
-function App() {
+class App extends Component
+{
+  constructor(){
+    super();
+    this.state= {
+      value : 0 ,
+      data : []
+    } 
+    //First Run Contructor Life Cycle
+    console.log("Constructor");
+  }
+
+  // static getDerivedStateFromProps(props,state){
+  //   console.log("Get Derived State From Props == >",state) 
+  //   return null 
+  // }
+
+
+  Increment  = () =>{
+    this.setState({
+     value : this.state.value + 1 
+    })
+  }
+
+  Decrement  = () =>{
+    this.setState({
+     value : this.state.value - 1 
+    })
+  }
+  
+componentDidMount(){
+  // console.log("componentDidMount")
+  // fetch('https://jsonplaceholder.typicode.com/todos/1')
+  // .then(response => response.json())
+  // .then(data => console.log(data))
+
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(
+    data =>   this.setState({
+      data : data
+    })
+  )
+}
+
+// shouldComponentUpdate(){
+//   return false
+// }
+
+// getSnapshotBeforeUpdate(preprops,prestate){
+//   console.log("Old Value :",prestate)
+//   return 10
+// }
+
+// componentDidUpdate(postprops,poststate,get){
+//   console.log("New Value",get)
+// }
+
+// componentWillUnmount(){
+//   console.log("components End")
+// }
+
+
+  
+  render(){
+    // console.log("data==>",this.state.data)
+    // console.log("Render")
+    // this.setState({
+    //   value : 'dtat'
+    // })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* <ul>
+        {this.state.data.map((v,i)=>{
+          return ( 
+            <li key={i}>{v.title}</li>
+          )
+        })}
+      </ul> */}
+      {this.state.value < 5 && <h1>{this.state.value}</h1> }
+      {/* <h1>{this.state.value}</h1> */}
+      <button onClick={()=>{this.Increment()}}>Increment</button>
+      <button onClick={()=>{this.Decrement()}}>Decrement</button>
+
+
     </div>
   );
 }
-
+  
+}
 export default App;
